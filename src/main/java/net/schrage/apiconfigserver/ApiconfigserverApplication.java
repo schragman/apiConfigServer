b4523a764743af5b215f3d91b0efc9899f46edfe
schragman <schragman@gmail.com>
2022-03-05 14:02:31 +0100

    Initial Commit

---
+++ src/main/java/net/schrage/apiconfigserver/ApiconfigserverApplication.java
@@ -0,0 +1,13 @@
+package net.schrage.apiconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiconfigserverApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiconfigserverApplication.class, args);
+  }
+
+}
---
+++ src/test/java/net/schrage/apiconfigserver/ApiconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package net.schrage.apiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiconfigserverApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
